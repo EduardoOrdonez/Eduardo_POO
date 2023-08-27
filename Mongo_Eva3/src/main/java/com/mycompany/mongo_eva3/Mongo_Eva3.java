d67c1bf8704fe0553fede62acfecd2d57d448d4d
EduardoOrdonez <118010755+EduardoOrdonez@users.noreply.github.com>
2023-08-27 18:04:55 -0500

    Add files via upload

---
+++ Mongo_Eva3/src/main/java/com/mycompany/mongo_eva3/Mongo_Eva3.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.mongo_eva3;
+
+import vista.Estudiante;
+
+
+public class Mongo_Eva3 {
+
+    public static void main(String[] args) {
+         new Estudiante().setVisible(true);
+         
+    }
+}
